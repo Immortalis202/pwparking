7d9ac6b7b00d59d89fa1e22e619d3da9b45c9431
emanueleblini <emanuele.blini04@gmail.com>
2024-06-17 21:58:25 +0200

    form validation

--- src/Components/SignIn/sign-in.js
+++ src/Components/SignIn/sign-in.js
@@ -5,27 +5,32 @@ const SignIn = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailValid, setEmailValid] = useState(false);
+    const [passwordValid, setPasswordValid] = useState(false);
 
     const validateEmail = () => {   
-        const pattern = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-    }
+        const pattern = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        setEmailValid(pattern.test(email));
+    }    
     const validatePassword = () => {       
-        const pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$";
-    }
+        const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+        setPasswordValid(pattern.test(password));
+    }    
     return (
-        <div>
+        <div className="form-container">
             <form method="post">
-                <div>
+                <h1>Login</h1>
+                <div className="input">
                     <label>Inserisci l'email</label>
                     <input type="email" placeholder="email" onChange={(e) => {setEmail(e.target.value); validateEmail();}} />
                 </div>
-                <div>
+                <div className="input">
                     <label>Inserisci la password</label>
                     <input type="password" placeholder="password" onChange={(e) => {setPassword(e.target.value); validatePassword();}} />
                 </div>
-                <div>
-                    <input type="submit" value="Invia" />
-                    <input type="reset" value="Cancella" />
+                <div className="button">
+                    <input type="submit" value="Invia" className="submit" disabled={!(emailValid && passwordValid)} />
+                    <input type="reset" value="Cancella" className="reset" />
                 </div>
             </form>
         </div>
