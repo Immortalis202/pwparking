fd0dc575a2df5ca7c8474c307c26dd08c391aab7
LeonardoPalaia <leonardo.palaia@itsrizzoli.it>
2024-06-17 15:42:30 +0200

    validate password anda email

--- src/Components/SignIn/sign-in.js
+++ src/Components/SignIn/sign-in.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React , {useState} from "react";
 import './sign-in.css';
 
 const SignIn = () => {
+    const validateEmail = () => {
+        const [email, setEmail] = useState('');
+        const pattern = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+    }
+    const validatePassword = () => {
+        const [password, setPassword] = useState('');
+        const pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$";
+    }
     return (
         <div>
-            <form>
+            <form method="post">
                 <div>
                     <label>Inserisci l'email</label>
-                    <input type="text" placeholder="email" />
+                    <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div>
                     <label>Inserisci la password</label>
-                    <input type="password" placeholder="password" />
+                    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                     <input type="submit" value="Invia" />
