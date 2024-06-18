70be867486d92d2f384f2e7083d477fb7f8afe53
emanueleblini <emanuele.blini04@gmail.com>
2024-06-18 09:40:52 +0200

    working validation

--- src/Components/SignIn/sign-in.js
+++ src/Components/SignIn/sign-in.js
@@ -1,32 +1,41 @@
-import React , {useState} from "react";
+import React, { useState, useEffect } from "react";
 import './sign-in.css';
 
 const SignIn = () => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [emailValid, setEmailValid] = useState(false);
     const [passwordValid, setPasswordValid] = useState(false);
 
+    useEffect(() => {
+        validateEmail();
+    }, [email]);
+
+    useEffect(() => {
+        validatePassword();
+    }, [password]);
+
     const validateEmail = () => {   
         const pattern = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
         setEmailValid(pattern.test(email));
     }    
+
     const validatePassword = () => {       
         const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
         setPasswordValid(pattern.test(password));
     }    
+
     return (
         <div className="form-container">
             <form method="post">
                 <h1>Login</h1>
                 <div className="input">
                     <label>Inserisci l'email</label>
-                    <input type="email" placeholder="email" onChange={(e) => {setEmail(e.target.value); validateEmail();}} />
+                    <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="input">
                     <label>Inserisci la password</label>
-                    <input type="password" placeholder="password" onChange={(e) => {setPassword(e.target.value); validatePassword();}} />
+                    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className="button">
                     <input type="submit" value="Invia" className="submit" disabled={!(emailValid && passwordValid)} />
@@ -38,4 +47,3 @@ const SignIn = () => {
 }
 
 export default SignIn;
-
