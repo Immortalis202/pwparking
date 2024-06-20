5b81073f78d449fc8ab679c92d00399c7af588ae
emanueleblini <emanuele.blini04@gmail.com>
2024-06-20 12:07:42 +0200

    form registrazione

--- src/Components/SignIn/sign-in.js
+++ src/Components/SignIn/sign-in.js
@@ -2,16 +2,22 @@ import React, { useState, useEffect } from "react";
 import './sign-in.css';
 
 const SignIn = () => {
+    const [nome, setNome] = useState('');
+    const [cognome, setCognome] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confermaPassword, setConfermaPassword] = useState('');
     const [emailValid, setEmailValid] = useState(false);
     const [passwordValid, setPasswordValid] = useState(false);
     const [user, setUser] = useState("");
 
     const login = (event) => {
         const form = event.target;
+        const nome = form.event.value;
+        const cognome = form.event.value;
         const email = form.email.value;
         const password = form.password.value;
+        const confermaPassword = form.confermaPassword.value;
     }
 
     useEffect(() => {
@@ -43,7 +49,15 @@ const SignIn = () => {
             ) : (
                 <div className="form-container">
                     <form method="post" onSubmit={login}>
-                        <h1>Login</h1>
+                        <h1>Sign Up</h1>
+                        <div className="input">
+                            <label>Inserisci l'email</label>
+                            <input type="text" name="nome" placeholder="nome" onChange={(e) => setNome(e.target.value)} />
+                        </div>
+                        <div className="input">
+                            <label>Inserisci l'email</label>
+                            <input type="text" name="cognome" placeholder="cognome" onChange={(e) => setCognome(e.target.value)} />
+                        </div>
                         <div className="input">
                             <label>Inserisci l'email</label>
                             <input type="email" name="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
@@ -51,6 +65,10 @@ const SignIn = () => {
                         <div className="input">
                             <label>Inserisci la password</label>
                             <input type="password" name="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className="input">
+                            <label>Conferma password</label>
+                            <input type="password" name="conferma-password" placeholder="conferma password" onChange={(e) => setConfermaPassword(e.target.value)} />
                         </div>
                         <div className="button">
                             <input type="submit" value="Invia" className="submit" disabled={!(emailValid && passwordValid)} />
