7880c24bab6cc9d829ebb4787e497b6d9d60804e
LeonardoPalaia <leonardo.palaia@itsrizzoli.it>
2024-06-21 09:15:01 +0200

    view password

--- src/Components/SignIn/sign-in.js
+++ src/Components/SignIn/sign-in.js
@@ -7,18 +7,32 @@ const SignIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confermaPassword, setConfermaPassword] = useState('');
+    const [nomeValid, setNomeValid] = useState(false);
+    const [cognomeValid, setCognomeValid] = useState(false);
     const [emailValid, setEmailValid] = useState(false);
     const [passwordValid, setPasswordValid] = useState(false);
+    const [confermaPasswordValid, setConfermaPasswordValid] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const [user, setUser] = useState("");
 
     const login = (event) => {
         const form = event.target;
-        const nome = form.event.value;
         const cognome = form.event.value;
+        const nome = form.event.value;
         const email = form.email.value;
         const password = form.password.value;
         const confermaPassword = form.confermaPassword.value;
+
+        
     }
+
+    useEffect(() => {
+        validateNome();
+    }, [nome]);
+
+    useEffect(() => {
+        validateCognome();
+    }, [cognome]);
 
     useEffect(() => {
         validateEmail();
@@ -28,6 +42,20 @@ const SignIn = () => {
         validatePassword();
     }, [password]);
 
+    useEffect(() => {
+        validateConfermaPassword();
+    }, [confermaPassword]);
+
+    const validateNome = () => {
+        const pattern = /^[a-zA-Z]+$/;
+        setNomeValid(pattern.test(nome));
+    }
+
+    const validateCognome = () => {
+        const pattern = /^[a-zA-Z]+$/;
+        setCognomeValid(pattern.test(cognome));
+    }
+    
     const validateEmail = () => {   
         const pattern = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
         setEmailValid(pattern.test(email));
@@ -37,10 +65,19 @@ const SignIn = () => {
         const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
         setPasswordValid(pattern.test(password));
     }    
+
+    const validateConfermaPassword = () => {
+        setConfermaPasswordValid(password === confermaPassword);
+    }
+
     const resetInput = () => {
+        setNomeValid(false);
+        setCognomeValid(false);
         setEmailValid(false);
         setPasswordValid(false);
+        setConfermaPassword(false);
     }
+    
 
     return (
         <div>
@@ -52,31 +89,49 @@ const SignIn = () => {
                         <h1>Sign Up</h1>
                         <div className="input">
                             <label>Inserisci il nome</label>
-                            <input type="text" name="nome" placeholder="nome" onChange={(e) => setNome(e.target.value)} />
+                            <input type="text" name="nome" placeholder="nome" onChange={(e) => setNome(e.target.value)} required/>
                         </div>
                         <div className="input">
                             <label>Inserisci il cognome</label>
-                            <input type="text" name="cognome" placeholder="cognome" onChange={(e) => setCognome(e.target.value)} />
+                            <input type="text" name="cognome" placeholder="cognome" onChange={(e) => setCognome(e.target.value)} required/>
                         </div>
                         <div className="input">
                             <label>Inserisci l'email</label>
-                            <input type="email" name="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+                            <input type="email" name="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} required/>
                         </div>
                         <div className="input">
                             <label>Inserisci la password</label>
-                            <input type="password" name="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+                            <input name="password" placeholder="password"  onChange={(e) => setPassword(e.target.value)} 
+                            
+                            id="pass"
+                            type={
+                                showPassword ? "text" : "password"
+                            }
+                            
+                            required/>
+                        </div>
+                        <div>
+                        <label for="check">Show Password</label>
+                            <input
+                                id="check"
+                                type="checkbox"
+                                value={showPassword}
+                                onChange={() =>
+                                    setShowPassword((prev) => !prev)
+                                }
+                            />
                         </div>
                         <div className="input">
                             <label>Conferma password</label>
-                            <input type="password" name="conferma-password" placeholder="conferma password" onChange={(e) => setConfermaPassword(e.target.value)} />
+                            <input type="password" name="conferma-password" placeholder="conferma password" onChange={(e) => setConfermaPassword(e.target.value)} required/>
                         </div>
                         <div className="button">
-                            <input type="submit" value="Invia" className="submit" disabled={!(emailValid && passwordValid)} />
+                            <input type="submit" value="Invia" className="submit" disabled={!(emailValid && passwordValid && nomeValid && cognomeValid && confermaPasswordValid)} />
                             <input type="reset" value="Cancella" className="reset" onClick={resetInput} />
                         </div>
                     </form>
                 </div>
-            )}
+            )}  
         </div>
     );
 }
